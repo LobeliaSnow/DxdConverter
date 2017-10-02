b452fba26203823d0a41e1cf10c36de593613e7e
雪瑠璃 <lobelia.snow@gmail.com>
2017-10-02 23:36:00 +0900

    情報保存部分を修正(くそ設計)

--- Source/FileController.hpp
+++ Source/FileController.hpp
@@ -3,6 +3,15 @@
 #include <string>
 
 namespace Dxd {
+	class FilePathControl {
+	public:
+		static std::string GetExtension(const std::string& file_path);
+		static std::string GetFilename(const std::string& file_path);
+		static std::string GetRootDirectory(const std::string& file_path);
+		static std::string GetParentDirectory(const std::string& file_path);
+		static std::string GetRelativeDirectory(const std::string& file_path);
+		//‘¼‚É‚à‚¢‚ë‚¢‚ë’Ç‰Á—\’è
+	};
 	class FileController {
 	public:
 		enum class OpenMode { NoOpen = -1, AppendBinaryPlus, WriteBinaryPlus, ReadBinaryPlus, AppendBinary, WriteBinary, ReadBinary, AppendPlus, WritePlus, ReadPlus, Append, Write, Read, };
